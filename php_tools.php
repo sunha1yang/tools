55e953c91d4cec20c8f743608be081313d77488d
sunha1yang <sunhaiyang@qudian.com>
2017-12-10 23:19:47 +0800

    upd: 首次添加

--- php_tools.php
+++ php_tools.php
@@ -10,4 +10,9 @@
   $mtime = date("Y-m-d H:i:s", strtotime("-3 month"));
 // php获取前一年的时间：
   $mtime = date("Y-m-d H:i:s", strtotime("-1 year"));
-
+// php获取今天的时时间
+  $start_time = date('Y-m-d H:00:00', strtotime('today'));
+  $end_time = date('Y-m-d', strtotime('today')).' 24:00:00';
+// php获取本月时间
+  $firstday = date("Y-m-01", strtotime(date("Y-m-d"))).' 00:00:00';
+  $lastday = date("Y-m-d", strtotime("$firstday +1 month -1 day")).' 24:00:00';
